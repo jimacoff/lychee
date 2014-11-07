342be282f55de04d9b2911b20b0d00b048efeb4f
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-11-08 08:58:54 +1000

    Fixes for rubocop violations

--- spec/support/models/concerns/concerns_shared.rb
+++ spec/support/models/concerns/concerns_shared.rb
@@ -1,15 +1,15 @@
 RSpec.shared_examples 'taggable' do
   it 'has defined tags field as text array' do
     expect(subject).to have_db_column(:tags)
-                                            .of_type(:text)
-                                            .with_options(array: false,
-                                                          default: [])
+      .of_type(:text)
+      .with_options(array: false,
+                    default: [])
   end
 end
 
 RSpec.shared_examples 'metadata' do
   it 'has defined metadata field as hstore' do
     expect(subject).to have_db_column(:metadata)
-                                                .of_type(:hstore)
+      .of_type(:hstore)
   end
 end
