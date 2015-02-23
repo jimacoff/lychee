a2cfd6dcc2a94305c5c8ccff873ab5542ad15c15
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-02-23 20:38:43 +1000

    Correctly reflect data model of tags

--- spec/support/models/concerns/concerns_shared.rb
+++ spec/support/models/concerns/concerns_shared.rb
@@ -3,7 +3,7 @@ RSpec.shared_examples 'taggable' do
 
   it 'has defined tags field as text array' do
     expect(subject).to have_db_column(:tags)
-      .of_type(:text).with_options(array: false, default: {})
+      .of_type(:text).with_options(array: false, default: [])
   end
 end
 
