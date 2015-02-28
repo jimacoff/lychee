9c5f6a1623587791cf23e22747dc997ad2f6defe
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-02-28 12:22:47 +1000

    Validate prioritized_countries
    If a whitelist is in use all prioritized_countries must appear in that
    list.
    
    Coversely if a blacklist is in use no prioritized_countries must not
    appear in that list.

--- app/models/site.rb
+++ app/models/site.rb
@@ -11,6 +11,7 @@ class Site < ActiveRecord::Base
   has_many :prioritized_countries
 
   validate :only_whitelisted_or_blacklisted_countries
+  validate :prioritized_countries_are_valid
 
   def restricts_countries?
     whitelisted_countries.present? || blacklisted_countries.present?
@@ -22,6 +23,30 @@ class Site < ActiveRecord::Base
     errors.add(:base, 'Cannot specify whitelisted and blacklisted countries')
   end
 
+  def prioritized_countries_are_valid
+    return unless whitelisted_countries.present? ||
+                  blacklisted_countries.present?
+
+    if  prioritized_not_in_whitelist?
+      errors.add(:base, 'All prioritized countries must appear in whitelist')
+    end
+
+    return unless prioritized_in_blacklist?
+    errors.add(:base, 'Prioritized countries must not appear in blacklist')
+  end
+
+  def prioritized_not_in_whitelist?
+    return false unless whitelisted_countries.present?
+
+    intersection(whitelisted_countries).count != prioritized_countries.count
+  end
+
+  def prioritized_in_blacklist?
+    return false unless blacklisted_countries.present?
+
+    intersection(blacklisted_countries).count > 0
+  end
+
   class << self
     def current
       Thread.current[:current_site]
@@ -30,5 +55,15 @@ class Site < ActiveRecord::Base
     def current=(site)
       Thread.current[:current_site] = site
     end
+  end
+
+  private
+
+  def intersection(country_type)
+    country_type.where(country_id: prioritized_country_ids)
+  end
+
+  def prioritized_country_ids
+    prioritized_countries.select(:country_id)
   end
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -26,6 +26,134 @@ RSpec.describe Site, type: :model do
                                                 site: subject)
         expect(subject).not_to be_valid
       end
+      context 'prioritized countries' do
+        let(:country1) { create :country }
+        let(:country2) { create :country }
+        let(:country3) { create :country }
+
+        context 'only prioritized countries specified' do
+          before do
+            subject.prioritized_countries << create(:prioritized_country,
+                                                    site: subject,
+                                                    country: country1)
+          end
+
+          it { is_expected.to be_valid }
+          it 'has prioritized_countries' do
+            expect(subject.prioritized_countries).to be_present
+          end
+          it 'has no whitelisted_countries' do
+            expect(subject.whitelisted_countries).not_to be_present
+          end
+        end
+
+        context 'with whitelist' do
+          RSpec.shared_examples 'expected country state' do
+            it 'has prioritized_countries' do
+              expect(subject.prioritized_countries).to be_present
+            end
+            it 'has whitelisted_countries' do
+              expect(subject.whitelisted_countries).to be_present
+            end
+          end
+
+          context 'all prioritized countries are whitelisted' do
+            before do
+              subject.whitelisted_countries << create(:whitelisted_country,
+                                                      site: subject,
+                                                      country: country1)
+              subject.whitelisted_countries << create(:whitelisted_country,
+                                                      site: subject,
+                                                      country: country2)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country1)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country2)
+            end
+
+            it { is_expected.to be_valid }
+            has_context 'expected country state'
+          end
+          context 'prioritized countries are whitelisted' do
+            before do
+              subject.whitelisted_countries << create(:whitelisted_country,
+                                                      site: subject,
+                                                      country: country1)
+              subject.whitelisted_countries << create(:whitelisted_country,
+                                                      site: subject,
+                                                      country: country2)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country1)
+            end
+
+            it { is_expected.to be_valid }
+            has_context 'expected country state'
+          end
+          context 'prioritized countries are not whitelisted' do
+            before do
+              subject.whitelisted_countries << create(:whitelisted_country,
+                                                      site: subject,
+                                                      country: country1)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country2)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country3)
+            end
+
+            it { is_expected.not_to be_valid }
+            has_context 'expected country state'
+          end
+        end
+
+        context 'with blacklist' do
+          RSpec.shared_examples 'expected country state' do
+            it 'has prioritized_countries' do
+              expect(subject.prioritized_countries).to be_present
+            end
+            it 'has blacklisted_countries' do
+              expect(subject.blacklisted_countries).to be_present
+            end
+          end
+
+          context 'prioritized countries are not blacklisted' do
+            before do
+              subject.blacklisted_countries << create(:blacklisted_country,
+                                                      site: subject,
+                                                      country: country1)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country2)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country3)
+            end
+
+            it { is_expected.to be_valid }
+            has_context 'expected country state'
+          end
+          context 'prioritized countries are blacklisted' do
+            before do
+              subject.blacklisted_countries << create(:blacklisted_country,
+                                                      site: subject,
+                                                      country: country1)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country1)
+              subject.prioritized_countries << create(:prioritized_country,
+                                                      site: subject,
+                                                      country: country2)
+            end
+
+            it { is_expected.not_to be_valid }
+            has_context 'expected country state'
+          end
+        end
+      end
     end
   end
 
@@ -45,14 +173,6 @@ RSpec.describe Site, type: :model do
       end
 
       it 'is restricted if blacklisted sites exist' do
-        subject.blacklisted_countries << create(:blacklisted_country,
-                                                site: subject)
-        expect(subject.restricts_countries?).to be
-      end
-
-      it 'is restricted if blacklisted and whitelisted sites exist' do
-        subject.whitelisted_countries << create(:whitelisted_country,
-                                                site: subject)
         subject.blacklisted_countries << create(:blacklisted_country,
                                                 site: subject)
         expect(subject.restricts_countries?).to be
