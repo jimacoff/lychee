f40a35c26654f62f8136f599a398e9ef720ffe16
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-03-14 10:46:08 +1000

    Remove unused columns from site definition

--- db/migrate/20150204095731_create_sites.rb
+++ db/migrate/20150204095731_create_sites.rb
@@ -2,11 +2,6 @@ class CreateSites < ActiveRecord::Migration
   def change
     create_table :sites do |t|
       t.string :name, null: false
-
-      t.text :whitelisted_countries, array: true, default:[]
-      t.text :blacklisted_countries, array: true, default:[]
-      t.text :priority_countries, array: true, default:[]
-
       t.timestamps null: false
     end
   end
