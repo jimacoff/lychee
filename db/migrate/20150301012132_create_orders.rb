cd94188ee6d64b964bbc023927314b789c8a77a6
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-03-29 15:32:23 +1000

    Fix migration bug for Order#price_cents

--- db/migrate/20150301012132_create_orders.rb
+++ db/migrate/20150301012132_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.integer :number, null: false
+      t.integer :price_cents, null: false
       t.string :status, null: false, limit: 255
 
       t.hstore :metadata, null: true
