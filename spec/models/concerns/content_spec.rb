db9f7063f20909a4dce69997f9c7cf9739972885
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-08-23 18:17:41 +1000

    Increase spec coverage for `Content` concern

--- spec/models/concerns/content_spec.rb
+++ spec/models/concerns/content_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Content do
 
   describe '#path' do
     it 'throws a failure when not overloaded' do
-      expect { subject.render }.to raise_exception('not implemented')
+      expect { subject.path }.to raise_exception('not implemented')
     end
   end
 end
