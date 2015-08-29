94be4df8d5d6989ff6de503c4ea0355bffd6ad43
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-08-29 10:14:14 +1000

    Add empty ShoppingCartController + route
    This provides some "real" verification that the `DasherizedRoutes` code
    works correctly.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ require 'dasherized_routes'
 Rails.application.routes.draw do
   self.class.include DasherizedRoutes
 
+  resource :shopping_cart, only: %i(show update destroy)
   resources :pages
 end
