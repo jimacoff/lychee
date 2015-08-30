e28ace6dc867b29b0f0bdb3ff0e00e33e1854eb4
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-08-30 14:32:05 +1000

    Add logic for applying basic cart updates
    Each cart operation pertains to a UUID. The UUID refers to a line item
    in the cart, so successive changes to the line item will retain the same
    UUID. The net effect is essentially that a shopping cart is an
    append-only list of operations.
    
    The primary goal here is to retain all operations that were performed on
    the cart so we can report on something like an item which was removed
    from the cart. The logic attempts not to lose any items that the
    customer wanted to add to their cart, so if something funky comes
    through from the frontend, the logic will attempt to make it valid and
    save it.

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -24,4 +24,29 @@ class ShoppingCart < ActiveRecord::Base
   has_many :shopping_cart_operations
 
   valhammer
+
+  def apply(opts)
+    (opts[:item_uuid] && apply_item_update(opts)) || apply_item_add(opts)
+  end
+
+  def contents
+  end
+
+  private
+
+  def apply_item_add(opts)
+    # TODO: Fold
+    shopping_cart_operations.create!(opts.merge(item_uuid: SecureRandom.uuid))
+  end
+
+  def apply_item_update(opts)
+    prev = shopping_cart_operations.where(opts.slice(:item_uuid))
+           .order('id desc').first
+
+    return nil if prev.nil? ||
+                  prev.product_id != opts[:product_id] ||
+                  prev.variant_id != opts[:variant_id]
+
+    shopping_cart_operations.create!(opts)
+  end
 end
--- spec/models/shopping_cart_spec.rb
+++ spec/models/shopping_cart_spec.rb
@@ -36,4 +36,116 @@ RSpec.describe ShoppingCart, type: :model, site_scoped: true do
     it_behaves_like 'workflow object', transitions: [:checkout, :cancel],
                                        state: :cancelled
   end
+
+  context '#apply' do
+    def operations
+      subject.shopping_cart_operations
+    end
+
+    def run
+      subject.apply(attrs)
+    end
+
+    shared_examples 'force a new uuid' do
+      it 'forces a new uuid' do
+        expect { run }.to change { operations.count }.by(1)
+
+        expected = attrs.merge(item_uuid: match(/[0-9a-f-]+/))
+        expect(operations.last).to have_attributes(expected)
+        expect(operations.last.item_uuid).not_to eq(attrs[:item_uuid])
+      end
+    end
+
+    shared_examples 'apply the operation successfully' do
+      it 'creates an operation' do
+        expect { run }.to change { operations.count }.by(1)
+
+        expected = attrs.merge(item_uuid: match(/[0-9a-f-]+/))
+        expect(operations.last).to have_attributes(expected)
+      end
+    end
+
+    shared_examples 'fail validation' do |error|
+      it 'raises a validation error' do
+        str = "Validation failed: #{error}"
+        expect { run }.to raise_error(ActiveRecord::RecordInvalid, str)
+      end
+    end
+
+    shared_examples 'an operable cart item' do
+      let!(:commodity) { create(kind) }
+      let(:commodity_attrs) { { :"#{kind}_id" => commodity.id } }
+
+      context 'with no uuid provided' do
+        let(:attrs) { commodity_attrs.merge(quantity: 1) }
+        include_examples 'apply the operation successfully'
+      end
+
+      context 'when providing a uuid' do
+        let(:attrs) do
+          commodity_attrs.merge(quantity: 1, item_uuid: SecureRandom.uuid)
+        end
+
+        context 'when the uuid is unknown' do
+          include_examples 'force a new uuid'
+        end
+
+        context 'when the uuid exists' do
+          let(:op_attrs) { attrs.merge(shopping_cart: subject, quantity: 3) }
+          let!(:op) { subject.shopping_cart_operations.create!(op_attrs) }
+
+          include_examples 'apply the operation successfully'
+
+          context 'with a mismatched variant' do
+            let(:other_variant) { create(:variant) }
+            let(:op_attrs) { attrs.merge(variant: other_variant, product: nil) }
+            include_examples 'force a new uuid'
+          end
+
+          context 'with a mismatched product' do
+            let(:other_product) { create(:product) }
+            let(:op_attrs) { attrs.merge(product: other_product, variant: nil) }
+            include_examples 'force a new uuid'
+          end
+        end
+
+        context 'with a nonexistent item' do
+          before { commodity.destroy }
+          include_examples 'fail validation',
+                           'Must belong to a product or variant'
+        end
+
+        context 'when a commodity from another site' do
+          let!(:commodity) { create(kind, site: create(:site)) }
+          include_examples 'fail validation',
+                           'Must belong to a product or variant'
+        end
+
+        context 'when product and variant are both specified' do
+          let(:attrs) do
+            { variant_id: variant.id, product_id: product.id,
+              quantity: 1, item_uuid: SecureRandom.uuid }
+          end
+          let(:product) { create(:product) }
+          let(:variant) { create(:variant) }
+
+          include_examples 'fail validation',
+                           'Cannot belong to a product and a variant'
+        end
+      end
+    end
+
+    context 'with a product' do
+      let(:kind) { :product }
+      it_behaves_like 'an operable cart item'
+    end
+
+    context 'with a variant' do
+      let(:kind) { :variant }
+      it_behaves_like 'an operable cart item'
+    end
+  end
+
+  context '#contents' do
+  end
 end
