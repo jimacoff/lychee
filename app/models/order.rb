fc4391a3fe9d668cffe42699e7f6a0f52f7fafbf
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2015-10-05 11:18:36 +1000

    Move order calculation logic into a concern
    The Order model was too long, and Rubocop was complaining after adding
    the order creation logic. Extracting calculation logic into a concern
    brought the model class back to a sensible size.

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,7 @@ class Order < ActiveRecord::Base
   include Taggable
 
   include OrderWorkflow
+  include OrderCalculation
 
   belongs_to :customer_address, class_name: 'Address'
   belongs_to :delivery_address, class_name: 'Address'
@@ -38,99 +39,10 @@ class Order < ActiveRecord::Base
     end
   end
 
-  def calculate_weight
-    change_weight(0) && return unless commodity_line_items.present?
-    change_weight(commodity_line_items.map(&:total_weight).sum)
-  end
-
-  def calculate_subtotal
-    change_subtotal(0) && return unless commodity_line_items.present?
-
-    if site.preferences.order_subtotal_include_tax
-      calculate_subtotal_tax_inclusive
-    else
-      calculate_subtotal_tax_exclusive
-    end
-  end
-
-  def calculate_total
-    unless commodity_line_items.present? && shipping_line_items.present?
-      fail 'attempt to calculate total with invalid state'
-    end
-
-    calculate_total_commodities
-    calculate_total_shipping
-    calculate_total_tax
-    finalise_total
-    calculate_tax_rates
-  end
-
   def create_line_items_from_bag(bag)
     bag.contents.values.each do |entry|
       attrs = entry.slice(:product, :variant, :quantity, :metadata)
       commodity_line_items.create!(attrs)
     end
-  end
-
-  private
-
-  def change_weight(weight)
-    self[:weight] = weight
-  end
-
-  def calculate_subtotal_tax_inclusive
-    change_subtotal(commodity_line_items.map(&:total).sum.cents)
-  end
-
-  def calculate_subtotal_tax_exclusive
-    change_subtotal(commodity_line_items.map(&:subtotal).sum.cents)
-  end
-
-  def calculate_total_commodities
-    change_total_commodities(commodity_line_items.map(&:total).sum.cents)
-  end
-
-  def calculate_total_shipping
-    change_total_shipping(shipping_line_items.map(&:total).sum.cents)
-  end
-
-  def calculate_total_tax
-    change_total_tax(shipping_line_items.map(&:tax).sum.cents +
-                     commodity_line_items.map(&:tax).sum.cents)
-  end
-
-  def calculate_tax_rates
-    order_taxes.destroy_all
-
-    tax_rate_totals.each do |tax_rate_id, total|
-      tax_rate = TaxRate.find(tax_rate_id)
-      order_taxes.create(tax_rate: tax_rate,
-                         used_tax_rate: tax_rate.rate,
-                         tax_amount: total.cents)
-    end
-  end
-
-  def tax_rate_totals
-    all_tax_rates.inject({}) do |h, lit|
-      h.merge(lit.tax_rate.id => lit.tax_amount) do |_key, current, additional|
-        current + additional
-      end
-    end
-  end
-
-  def all_tax_rates
-    commodity_line_item_taxes + shipping_line_item_taxes
-  end
-
-  def commodity_line_item_taxes
-    commodity_line_items.flat_map(&:line_item_taxes)
-  end
-
-  def shipping_line_item_taxes
-    shipping_line_items.flat_map(&:line_item_taxes)
-  end
-
-  def finalise_total
-    change_total(total_commodities.cents + total_shipping.cents)
   end
 end
