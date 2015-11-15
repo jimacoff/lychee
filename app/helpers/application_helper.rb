75830e48de19c6e8f3d6175ce86b0006b2262108
Bradley Beddoes <bradleybeddoes@gmail.com>
2015-11-15 10:50:01 +1000

    Add responsive_image helper for dynamic views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def icon_tag(icon)
-    content_tag('i', '', class: "fa fa-#{icon}")
+  def responsive_image(image_instance, sizes, classes)
+    default_image = image_instance.image.image_files.default_image
+    options = { src: default_image.uri_path,
+                srcset: image_instance.image.srcset_path,
+                sizes: sizes, alt: image_instance.description,
+                class: classes }
+    content_tag(:img, nil, options)
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe ApplicationHelper, type: :helper do
-  context '#icon_tag' do
-    it 'returns a FontAwesome icon tag' do
-      expect(helper.icon_tag('xyz')).to eq('<i class="fa fa-xyz"></i>')
+RSpec.describe ApplicationHelper, type: :helper, site_scoped: true do
+  describe '#responsive_image' do
+    let(:sizes) { '(min-width 40em) 100vw, 50vw' }
+    let(:classes) { Faker::Lorem.word.to_s }
+    let(:ii) { create :image_instance, :routable }
+    let(:default_image) { ii.image.image_files.default_image }
+    let(:expected_html_src) do
+      "<img src=\"#{default_image.uri_path}\" " \
+      "srcset=\"#{ii.image.srcset_path}\" " \
+      "sizes=\"#{sizes}\" alt=\"#{ii.description}\" " \
+      "class=\"#{classes}\"></img>"
+    end
+    it 'provides an img tag with responsive elements' do
+      expect(helper.responsive_image(ii, sizes, classes))
+        .to eq(expected_html_src)
     end
   end
 end
